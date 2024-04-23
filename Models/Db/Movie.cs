933542a2888ea86e8ee6ae5ef30720faa556fa8d
marlind24 <marika.lindqvist2024@outlook.com>
2024-04-23 11:45:03 +0200

    Adding in MovieModel

---
+++ Models/Db/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VanillaMovieShop.Models.Db
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [Data]
+        public string Title { get; set; }
+
+        [Required] 
+        public string Director { get; set; }
+        
+        [Required]
+        [DataType(DataType.DateTime)]
+        [Display()]
+        public int ReleaseYear { get; set; }
+        
+        [Required]
+        [DataType(DataType.Currency)]
+        public int Price { get; set; }
+        
+        [Required]
+        public string? Genre { get; set; }
+        
+        [Required] 
+        public string? Actors { get; set; }
+
+    }
+}
