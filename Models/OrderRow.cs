b2a6fe7ee4f6a163811ed7e36c7179cf39a74394
Mark Aurelius <tjader.marcus@gmail.com>
2024-04-23 11:49:15 +0200

    Added OrderRows with commented virtuals

---
+++ Models/OrderRow.cs
@@ -0,0 +1,13 @@
+﻿namespace VanillaMovieShop.Models
+{
+    public class OrderRow
+    {
+        public int Id { get; set; }
+        public string OrderId { get; set; }
+        public string MovieId { get; set; }
+        public decimal Price { get; set; }
+
+        //public virtual Movie Movie { get; set; }
+        //Spublic virtual Order Order { get; set; }
+    }
+}
