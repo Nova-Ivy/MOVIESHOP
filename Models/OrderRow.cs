cbb394c7cf02b3afb10e377d9cea4b45d6965fd6
Mark Aurelius <tjader.marcus@gmail.com>
2024-04-23 12:03:33 +0200

    Using Dataannotations and Required Currency

--- Models/OrderRow.cs
+++ Models/OrderRow.cs
@@ -1,10 +1,13 @@
-﻿namespace VanillaMovieShop.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace VanillaMovieShop.Models
 {
     public class OrderRow
     {
         public int Id { get; set; }
         public string OrderId { get; set; }
         public string MovieId { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
 
         //public virtual Movie Movie { get; set; }
