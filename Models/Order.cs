b685e045de239cf4afa4ccf890464cbb7bcdfd5d
Mehreen sohail <mehreensoh@gmail.com>
2024-04-23 11:39:58 +0200

    OrderUpdate

---
+++ Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace VanillaMovieShop.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{MM/dd/yyyy HH:mm}")]
+        public DateTime OrderDate { get; set; }
+
+        [Required]
+        public int CustomerId { get; set; }
+
+    }
+}
