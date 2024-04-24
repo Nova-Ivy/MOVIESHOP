c8d6292513cc6abfb4a78480208611483b91905f
Mehreen sohail <mehreensoh@gmail.com>
2024-04-24 11:28:59 +0200

    update MovieService

---
+++ Services/MovieService.cs
@@ -0,0 +1,56 @@
+﻿using VanillaMovieShop.Data;
+using VanillaMovieShop.Models.Db;
+
+namespace VanillaMovieShop.Services
+{
+    public class MovieService
+    {
+        private readonly VanillaDbContext _db;
+        private readonly IConfiguration _configuration;
+        public MovieService(VanillaDbContext db, IConfiguration configuration)
+        {
+            _db = db;
+            _configuration = configuration;
+        }
+
+        public List<Movie> GetMovies()
+        {
+            var movies = _db.Movies.ToList();
+            return movies;
+        }
+
+        public Movie GetMovieById(int id)
+        {
+            var movie = _db.Movies.FirstOrDefault(m => m.Id == id);
+            return movie;
+        }
+
+        public List<Movie> GetMoviesByLeatest()
+        {
+            var movies = _db.Movies.OrderByDescending(m => m.ReleaseYear).ToList();
+            return movies;
+        }
+        public List<Movie> GetMoviesByOldest()
+        {
+            var movies = _db.Movies.OrderBy(m => m.ReleaseYear).ToList();
+            return movies;
+        }
+        public List<Movie> GetCheapestMovies()
+        {
+            var movies = _db.Movies.OrderBy(m => m.ReleaseYear).ToList();
+            return movies;
+        }
+
+        public List<Movie> GetMostPopularMovie()
+        {
+           var topMostPopularMovie = _db.Movies.OrderByDescending(m => m.ReleaseYear).FirstOrDefault();
+            return GetMostPopularMovie();
+        }
+
+        public void AddMovie(Movie movie)
+        {
+            _db.Movies.Add(movie);
+            _db.SaveChanges();
+        }
+    }
+}
