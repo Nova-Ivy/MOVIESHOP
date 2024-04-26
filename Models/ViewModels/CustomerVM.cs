bdc8a9cafbc236a1b5686f0f7298133c808d08b5
marlind24 <marika.lindqvist2024@outlook.com>
2024-04-26 08:53:23 +0200

    //Added views to index, delete, create, edit customer

---
+++ Models/ViewModels/CustomerVM.cs
@@ -0,0 +1,9 @@
+﻿using VanillaMovieShop.Models.Db;
+
+namespace VanillaMovieShop.Models.ViewModels
+{
+    public class CustomerVM
+    {
+       public List <Customer> Customers { get; set; } = new List <Customer> ();
+    }
+}
--- Models/ViewModels/MoviesVM.cs
+++ Models/ViewModels/MoviesVM.cs
@@ -6,6 +6,5 @@ namespace VanillaMovieShop.Models.ViewModels
     {
         public List<Movie>Movies { get; set; } = new List<Movie>();
         public List<Customer> Customers { get; set; } = new List<Customer>();
-
     }
 }
